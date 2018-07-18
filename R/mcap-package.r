cae0645dcbf622bcbf387096ea080751b9b6b688
Bernd Taschler <bernd.taschler@gmail.com>
2018-07-18 10:31:51 +0200

    add package level documentation

---
+++ R/mcap-package.r
@@ -0,0 +1,10 @@
+#' mcap: Model-based clustering in very high dimensions via adaptive projections.
+#' 
+#' Model-based clustering in very high dimensions (especially p >> n) via adaptive projections. 
+#' Clustering is based on Gaussian mixture models in a lower dimensional (projected) space.
+#' Projection dimension is set adaptively based on a cluster stability criterion.
+#' Available projection variants (so far) include PCA and Random Projections.
+#'
+#' @name mcap
+#' @docType package
+NULL
