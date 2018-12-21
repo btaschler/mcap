43953e46b35560c3411e56bc78f977c639e77056
Bernd Taschler <bernd.taschler@gmail.com>
2018-12-21 11:31:29 +0100

    initial commit

---
+++ R/CentrePerGroup.R
@@ -0,0 +1,36 @@
+
+CentrePerGroup <- function(xx, true_labels = numeric(nrow(xx))){
+  #' Centre a data matrix to mean zero (per group)
+  #'
+  #' Mean centering of an input matrix. When true labels are provided, the 
+  #' groups/clusters are centred individually. 
+  #' 
+  #' @author Bernd Taschler \email{bernd.taschler@dzne.de}
+  #' 
+  #' @param xx The data matrix (n x p).
+  #' @param true_labels Vector of true cluster assignments. Default: 0 (all 
+  #'                    elements are treated as belonging to one group).
+  #' 
+  #' @return @param xx_centred Mean centred data matrix.
+  #' @export
+  
+  # preliminaries ------------------------------------------------------------
+  n <- nrow(xx)
+  p <- ncol(xx)
+  xx_centred <- matrix(numeric(), n, p)
+  
+  ## get group labels
+  labels_names <- unique(true_labels)
+  k <- length(labels_names)
+  
+  ## mean centre each group separately
+  for(j in seq(k)){
+    curr_k_idx <- which(true_labels == labels_names[j])
+    curr_k_data <- xx[curr_k_idx,]
+    xx_centred[curr_k_idx,] <- curr_k_data - 
+      t(matrix(rep(colMeans(curr_k_data), nrow(curr_k_data)), 
+               ncol(curr_k_data), nrow(curr_k_data)))        
+  }    
+  
+  return(xx_centred)
+}
---
+++ R/Rand1.R
@@ -0,0 +1,13 @@
+
+Rand1 <- function(n){
+  #' Return a random sign vector
+  #'
+  #' Create a random sign vector of length `n` (iid ~ +1/-1).
+  #' 
+  #' @param n Number of elements
+  #' 
+  #' @return Sign vector of length `n`.
+  #' @export
+  
+  return((round(runif(n)) * 2) - 1)
+}
